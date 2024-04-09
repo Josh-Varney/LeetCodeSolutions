710cc1dc17ae4c068392951a61d2102acd48beda
Joshua Varney <up2138017@myport.ac.uk>
2024-04-09 18:28:00 +0100

    Hashmap

---
+++ C++/Leetcode/Hashmap/isomorphicStrings.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <map>
+
+class Solution {
+    public:
+        bool isIsomorphic(std::string s, std::string t){
+            if (s.size() != t.size()){
+                return false;
+            }
+            
+            std::map<char, int> sMap;
+            std::map<char, int> tMap;
+            for (int i=0; i<s.size(); ++i){
+                sMap[s[i]]++;
+                tMap[t[i]]++;
+            }
+
+            for ()
+        }
+
+    private:
+        void printMap(std::map<char, int> map){
+            for (const auto& pair : map){
+                std::cout << pair.first << " " << pair.second << std::endl;
+            }
+        }
+};
+
+int main(){
+    Solution solution_instance;
+    solution_instance.isIsomorphic("egg", "add");
+}
---
+++ Python/Leetcode/HashMap/longestSequence.py
@@ -0,0 +1,30 @@
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        if not nums:
+            return 0
+        
+        maxCount = 1
+        count = 1
+        seen = set()
+        
+        nums.sort()
+        for r in range(len(nums)):
+            
+            if nums[r] in seen:
+                continue
+            elif (nums[r] - 1) in seen:
+                count += 1
+                seen.add(nums[r])
+            else:
+                maxCount = max(maxCount, count)
+                count = 1
+                seen.add(nums[r])
+
+        maxCount = max(maxCount, count)  # Update maxCount after the loop
+        return maxCount
+        
+        
+        
+if __name__ == '__main__':
+    solution_instance = Solution()
+    print(solution_instance.longestConsecutive([0,3,7,2,5,8,4,6,0,1]))
